d15dd4235d3b7e83b848e0c64b9f9eb7ad148a6c
Purnya <130914821+Psml03@users.noreply.github.com>
2024-10-27 21:39:29 +0100

    Update get_kern.py, To resolve the TabError: inconsistent use of tabs and spaces in indentation

--- get_kern.py
+++ get_kern.py
@@ -1,5 +1,7 @@
 import os
 from subprocess import check_call, check_output, call
+
+# Create a file to store kernel versions
 fp = open('raw_kern', 'w')
 call(['ls', '/boot'], stdout=fp)
 
@@ -9,31 +11,33 @@ with open('raw_kern', 'r') as fp:
     for l in lines:
         if 'vmlinuz' in l:
             full_name = l.strip('vmlinuz-').strip()
-            #print full_name
+            # print full_name
             num_name = full_name.strip('-generic')
             if '-' in num_name:
-		version_seg = num_name.split('-')
+                version_seg = num_name.split('-')
             elif 'c' in num_name:
-            	version_seg = num_name.split('c')
+                version_seg = num_name.split('c')
             elif 'd' in num_name:
-            	version_seg = num_name.split('d')
+                version_seg = num_name.split('d')
 
             first_versions = version_seg[0].split('.')
-            #second_version = version_seg[1]
             ident = []
-	    print version_seg
+            print(version_seg)  # Added parentheses for compatibility with Python 3
             for s in first_versions:
                 ident.append(int(s))
             length = len(ident)
             assert length == 3
-            #ident.append(int(second_version))
             ident.append(full_name)
             versions.append(ident)
-versions = sorted(versions, key = lambda x : (x[0], x[1], x[2]))
 
+# Sort the versions
+versions = sorted(versions, key=lambda x: (x[0], x[1], x[2]))
+
+# Write the sorted kernel versions to a file
 with open('kern_list', 'w') as fp:
     for v in versions:
-        print v
-        fp.write(v[3]+'\n')
+        print(v)  # Added parentheses for compatibility with Python 3
+        fp.write(v[3] + '\n')
 
+# Remove the temporary file
 os.remove('raw_kern')
