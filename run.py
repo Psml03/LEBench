69dfedef1951a4fb1699db428608d175a70a08df
Purnya <130914821+Psml03@users.noreply.github.com>
2024-10-28 15:45:43 +0100

    Update run.py, only run for current kernel version

--- run.py
+++ run.py
@@ -1,88 +1,24 @@
 #!/usr/bin/env python
 import os
-import signal
 import platform
 import sys
-from subprocess import check_call, check_output, call
+from subprocess import call
 from os.path import join
 from datetime import datetime
 
 DEBUG = True
 
-GRUB_CFG_FILE = '/boot/grub/grub.cfg'
-GRUB_FILE = '/etc/default/grub'
-
-# Set WORKING_DIR without the prefix
+# Set the working directory
 WORKING_DIR = '/home/purnya/benchmark/LEBench'
-KERN_INDEX_FILE = WORKING_DIR + '/iteration'  # Ensure this is correct
-LOCAL_GRUB_FILE = WORKING_DIR + '/grub'  # Ensure this is correct
-KERN_LIST_FILE = WORKING_DIR + '/kern_list'  # Ensure this is correct
-RESULT_DIR = WORKING_DIR + '/RESULT_DIR/'  # Ensure this is correct
-TEST_DIR = WORKING_DIR + '/TEST_DIR/'  # Ensure this is correct
+KERN_INDEX_FILE = WORKING_DIR + '/iteration' 
+LOCAL_GRUB_FILE = WORKING_DIR + '/grub'  
+KERN_LIST_FILE = WORKING_DIR + '/kern_list'  
+RESULT_DIR = WORKING_DIR + '/RESULT_DIR/'  
+TEST_DIR = WORKING_DIR + '/TEST_DIR/'  
 TEST_NAME = 'OS_Eval'
 
-""" Set temporary
-"""
-os.environ['LEBENCH_DIR'] = WORKING_DIR  # Use the defined WORKING_DIR
-
-""" Grabs the ith kernel from KERN_LIST_FILE.
-"""
-def get_kern_list(idx):
-    with open(KERN_LIST_FILE, 'r') as fp:
-        lines = fp.readlines()
-
-        if -1 < idx < len(lines):
-            return lines[idx].strip()
-        elif idx >= len(lines):
-            print('[INFO] LEBench run concluded, finished testing on ' + str(len(lines)) + ' kernels.')
-            os.remove(KERN_INDEX_FILE)
-            sys.exit(0)
-        else:
-            raise ValueError('Kernel index out of range, '
-                             'expect index to be between 0 and ' + str(len(lines)))
-
-""" Modifies the grub file to boot into the target kernel the next time.
-"""
-def generate_grub_file(f, target_kern):
-    if DEBUG: print('[DEBUG]    Preparing grub for kernel: ' + target_kern)
-    if DEBUG: print('[DEBUG]--------------------------------------------------')
-
-    if not os.path.exists(f):
-        raise ValueError("File %s does not exist." % f)
-
-    kern_image_name = 'vmlinuz-%s' % target_kern
-    if not os.path.exists(os.path.join('/', 'boot', kern_image_name)):
-        raise ValueError('Kernel image %s does not exist' % kern_image_name)
-
-    print('[INFO] Setting boot version to ' + target_kern + '.')
-    with open(f, 'r') as fp:
-        lines = fp.readlines()
-
-        for idx, line in enumerate(lines):
-            if line.startswith('GRUB_DEFAULT'):
-                line = 'GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux %s"\n' % target_kern
-                lines[idx] = line
-
-    # Correct the path to the template grub file here
-    template_grub_file = WORKING_DIR + '/template/grub'  # Specify the full path here
-    with open(template_grub_file, 'w+') as fp:  # Ensure you open the template grub file
-        fp.writelines(lines)
-
-    return True
-
-"""Sets up grub using configured grub file and shell cmds.
-"""
-def install_grub_file():
-    if DEBUG: print("[DEBUG] Copying GRUB config to %s" % GRUB_FILE)
-    call(['sudo', 'cp', LOCAL_GRUB_FILE, GRUB_FILE])
-    if DEBUG: print("[DEBUG] Configuring boot")
-    call(['sudo', 'grub-install', '--force', '--target=i386-pc', '/dev/sda1'])
-    if DEBUG: print("[DEBUG] Making grub config")
-    call(['sudo', 'grub-mkconfig', '-o', GRUB_CFG_FILE])
-
-def restart():
-    print('[INFO] Restarting the machine now.')
-    call(['sudo', 'reboot'])
+# Set temporary
+os.environ['LEBENCH_DIR'] = WORKING_DIR 
 
 """ Running the LEBench tests for the current kernel version.
 """
@@ -129,64 +65,6 @@ if __name__ == '__main__':
     if not os.geteuid() == 0:
         raise Exception('This script should be run with "sudo".')
 
-    try:
-        WORKING_DIR = os.environ['LEBENCH_DIR']
-    except:
-        raise ValueError('$LEBENCH_DIR is not set. Example: "/home/username/LEBench/".')
-
-    if 'LEBench' not in WORKING_DIR:
-        raise ValueError('$LEBENCH_DIR should point to the directory containing LEBench. Example: "/home/username/LEBench/".')
-
-    # Update paths to use WORKING_DIR correctly
-    KERN_INDEX_FILE = WORKING_DIR + '/iteration'
-    LOCAL_GRUB_FILE = WORKING_DIR + '/grub'
-    KERN_LIST_FILE = WORKING_DIR + '/kern_list'
-    RESULT_DIR = WORKING_DIR + '/RESULT_DIR/'
-    TEST_DIR = WORKING_DIR + '/TEST_DIR/'
-
-    if not os.path.exists(KERN_LIST_FILE):
-        raise IOError('Cannot open "kern_list" file. If it\'s not present, '
-                      'run "get_kern.py" to generate this file by grepping all install kernels.')
-
-    with open(KERN_LIST_FILE, 'r') as fp:
-        lines = fp.readlines()
-        if len(lines) == 0:
-            raise ValueError('"kern_list" file is empty, '
-                             'run "get_kern.py" to generate this file by grepping all install kernels.')
-
-    # For running LEBench on one specified kernel version.
-    if len(sys.argv) > 1:
-        kern_version = sys.argv[1]
-        print("[INFO] Configuring to boot into " + kern_version + ".")
-        generate_grub_file(WORKING_DIR + '/template/grub', kern_version)
-        install_grub_file()
-        sys.exit(0)
-
-    # For running LEBench on a list of specified kernel versions.
-    if not os.path.exists(KERN_INDEX_FILE):#
-        print("This is an immediate output.", flush=True)
-        with open(KERN_INDEX_FILE, 'w') as f:
-            f.write("-1\n")
-
-    with open(KERN_INDEX_FILE, 'r') as f:
-        kern_idx = int(f.read())
-    next_kern_idx = kern_idx + 1
-    if DEBUG: print('[DEBUG] Running at kernel index: ' + str(kern_idx))
-    
-    with open(KERN_INDEX_FILE, 'w') as fp:
-        fp.write(str(next_kern_idx).strip())
-
-    if DEBUG: print('[DEBUG] Done writing kernel index %d for the next iteration' % next_kern_idx + '.')
-
-    if next_kern_idx == 0:
-        # Need to boot into the right kernel version first.
-        print('[INFO] LEBench tests will start after booting into the first kernel.')
-    else:
-        # We are at the right kernel version, actually run LEBench.
-        run_bench()
-
-    if DEBUG: print('[DEBUG] Preparing to modify grub.')
-    if generate_grub_file(WORKING_DIR + '/template/grub', get_kern_list(next_kern_idx)):
-        install_grub_file()
-        if DEBUG: print('[DEBUG] Done configuring grub for the next kernel.')
-        restart()
+    # Directly run the benchmark without modifying GRUB
+    print("[INFO] Running benchmark on the current kernel version.")
+    run_bench()
